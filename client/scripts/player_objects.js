f58e872037775ff2011a8446f0317780a9e9bf0c
iTecAI <matteovh@gmail.com>
2021-02-26 08:50:47 -0500

    refactor player into multiple files

---
+++ client/scripts/player_objects.js
@@ -0,0 +1,316 @@
+function draw_obscure(obj) {
+    return $('<div class="object obscure"></div>')
+        .toggleClass('dm', current_cmp_data.dms.includes(uid))
+        .attr('data-oid', obj.id)
+        .css({
+            top: obj.position.y + '%',
+            left: obj.position.x + '%',
+            width: obj.data.width + '%',
+            height: obj.data.height + '%'
+        })
+        .on('click', function (event) {
+            if (event.button != 0) {
+                return;
+            }
+            if (current_tool == 'obscure') {
+                post(
+                    '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(event.delegateTarget).attr('data-oid') + '/delete/',
+                    function () { }
+                );
+            }
+        })
+        .on('ctx:delete', function (event) {
+            post(
+                '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/delete/',
+                function () { }
+            );
+        });
+}
+
+function draw_note(obj) {
+    if (!obj.data.player_visible && !current_cmp_data.dms.includes(uid)) {
+        return;
+    }
+    var note = $('<span class="note-content" contenteditable="true"></span>')
+        .text(obj.data.content);
+
+    return $('<div class="object note" data-moving="false"></div>')
+        .append(note)
+        .css({
+            color: obj.data.color,
+            top: obj.position.y + '%',
+            left: obj.position.x + '%'
+        })
+        .attr('data-oid', obj.id)
+        .toggleClass('player-visible', obj.data.player_visible)
+        .toggleClass('player-invisible', !obj.data.player_visible)
+        .on('mousemove', function (e) {
+            if (current_tool == 'note' && current_cmp_data.dms.includes(uid)) {
+                $('.note').attr('contenteditable', 'true');
+            } else {
+                $('.note').attr('contenteditable', 'false');
+            }
+            if ($(this).attr('data-moving') == 'true') {
+                var pos = getPercentPosition(e.clientX, e.clientY);
+                $(this).css({
+                    top: pos.y + '%',
+                    left: pos.x + '%'
+                });
+            }
+        })
+        .on('mousedown', function (event) {
+            if (event.button == 0) {
+                if (current_tool == 'move' && current_cmp_data.dms.includes(uid)) {
+                    $(this).attr('data-moving', 'true');
+                }
+            }
+        })
+        .on('mouseup', function (event) {
+            if ($(this).attr('data-moving') != 'true') {
+                return;
+            }
+            $(this).attr('data-moving', 'false');
+            var pos = getPercentPosition(event.clientX, event.clientY);
+            post(
+                '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(event.delegateTarget).attr('data-oid') + '/move/',
+                function () { },
+                {}, pos
+            );
+        })
+        .on('blur paste', function (event) {
+            if ($(this).children('.note-content').text().length == 0) {
+                post(
+                    '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(event.delegateTarget).attr('data-oid') + '/delete/',
+                    function () { }
+                );
+            } else {
+                post(
+                    '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(event.delegateTarget).attr('data-oid') + '/modify/',
+                    function () { },
+                    {}, {
+                    path: 'content',
+                    value: $(this).children('.note-content').text()
+                }
+                );
+            }
+        })
+        .on('keypress', function (event) {
+            if (event.originalEvent.key == 'Enter') {
+                event.preventDefault();
+                $(this).trigger('click');
+            }
+        })
+        .on('ctx:delete', function () {
+            post(
+                '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/delete/',
+                function () { }
+            );
+        })
+        .on('ctx:visibility_off', function () {
+            post(
+                '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/modify/',
+                function () { }, {}, {
+                path: 'player_visible',
+                value: false
+            }
+            );
+        })
+        .on('ctx:visibility_on', function () {
+            post(
+                '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/modify/',
+                function () { }, {}, {
+                path: 'player_visible',
+                value: true
+            }
+            );
+        })
+        .on('ctx:color_cycle', function () {
+            if (note_colors.indexOf(current_map_data.objects[$(this).attr('data-oid')].data.color) == note_colors.length - 1) {
+                var c = note_colors[0];
+            } else {
+                var c = note_colors[note_colors.indexOf(current_map_data.objects[$(this).attr('data-oid')].data.color) + 1];
+            }
+            post(
+                '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/modify/',
+                function () { }, {}, {
+                path: 'color',
+                value: c
+            }
+            );
+        });
+}
+
+function draw_shape(obj) {
+    if (!obj.data.player_visible && !current_cmp_data.dms.includes(uid)) {
+        return;
+    }
+    if (obj.data.shape_type == 'rectangle' || obj.data.shape_type == 'circle') {
+        return $('<div class="object shape"></div>')
+            .addClass(obj.data.shape_type)
+            .toggleClass('dm', current_cmp_data.dms.includes(uid))
+            .attr('data-oid', obj.id)
+            .css({
+                top: obj.position.y + '%',
+                left: obj.position.x + '%',
+                width: obj.data.width + '%',
+                height: obj.data.height + '%',
+                'background-color': obj.data.color
+            })
+            .on('ctx:delete', function (event) {
+                post(
+                    '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/delete/',
+                    function () { }
+                );
+            })
+            .append(
+                $('<span class="measurement"></span>')
+                    .text(
+                        String(Math.round(obj.data.width * (current_map_data.dimensions.scale * current_map_data.dimensions.columns) / 100)) +
+                        ' x ' +
+                        String(Math.round(obj.data.height * (current_map_data.dimensions.scale * current_map_data.dimensions.rows) / 100))
+                    )
+            )
+            .toggleClass('player-visible', obj.data.player_visible)
+            .toggleClass('player-invisible', !obj.data.player_visible)
+            .on('mousemove', function (e) {
+                if ($(this).attr('data-moving') == 'true') {
+                    var pos = getPercentPosition(e.clientX, e.clientY);
+                    $(this).css({
+                        top: pos.y + '%',
+                        left: pos.x + '%'
+                    });
+                }
+            })
+            .on('mousedown', function (event) {
+                if (event.button == 0) {
+                    if (current_tool == 'move') {
+                        $(this).attr('data-moving', 'true');
+                    }
+                }
+            })
+            .on('mouseup', function (event) {
+                if ($(this).attr('data-moving') != 'true') {
+                    return;
+                }
+                $(this).attr('data-moving', 'false');
+                var pos = getPercentPosition(event.clientX, event.clientY);
+                post(
+                    '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(event.delegateTarget).attr('data-oid') + '/move/',
+                    function () { },
+                    {}, pos
+                );
+            })
+            .on('ctx:visibility_off', function () {
+                post(
+                    '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/modify/',
+                    function () { }, {}, {
+                    path: 'player_visible',
+                    value: false
+                }
+                );
+            })
+            .on('ctx:visibility_on', function () {
+                post(
+                    '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/modify/',
+                    function () { }, {}, {
+                    path: 'player_visible',
+                    value: true
+                }
+                );
+            })
+            .on('ctx:color_cycle', function () {
+                if (note_colors.indexOf(current_map_data.objects[$(this).attr('data-oid')].data.color) == note_colors.length - 1) {
+                    var c = note_colors[0];
+                } else {
+                    var c = note_colors[note_colors.indexOf(current_map_data.objects[$(this).attr('data-oid')].data.color) + 1];
+                }
+                post(
+                    '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/modify/',
+                    function () { }, {}, {
+                    path: 'color',
+                    value: c
+                }
+                );
+            });
+    }
+}
+
+function draw_character(obj) {
+    return $('<div class="object character"></div>')
+        .toggleClass('owned', uid == obj.data.owner)
+        .attr('data-oid', obj.id)
+        .attr('data-cid', obj.data.char_id)
+        .append(
+            $('<span class="char-token"></span>')
+                .append($('<img>').attr('src', cond(
+                    current_cmp_data.character_data[obj.data.char_id].appearance.image == null,
+                    'assets/logo.png',
+                    current_cmp_data.character_data[obj.data.char_id].appearance.image
+                )))
+        )
+        .append($('<span class="char-name"></span>').text(current_cmp_data.character_data[obj.data.char_id].name))
+        .css({
+            top: obj.position.y + '%',
+            left: obj.position.x + '%'
+        })
+        .on('mousemove', function (e) {
+            if ($(this).attr('data-moving') == 'true') {
+                var pos = getPercentPosition(e.clientX, e.clientY);
+                $(this).css({
+                    top: pos.y + '%',
+                    left: pos.x + '%'
+                });
+            }
+        })
+        .on('mousedown', function (event) {
+            if (event.button == 0 && ($(this).hasClass('owned') || current_cmp_data.dms.includes(uid))) {
+                if (current_tool == 'move') {
+                    $(this).attr('data-moving', 'true');
+                }
+            }
+        })
+        .on('mouseup', function (event) {
+            if ($(this).attr('data-moving') != 'true') {
+                return;
+            }
+            $(this).attr('data-moving', 'false');
+            var pos = getPercentPosition(event.clientX, event.clientY);
+            post(
+                '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(event.delegateTarget).attr('data-oid') + '/move/',
+                function () { },
+                {}, pos
+            );
+        })
+        .on('ctx:delete', function (event) {
+            post(
+                '/campaign/' + current_cmp_data.id + '/maps/' + current_map_data.id + '/objects/' + $(this).attr('data-oid') + '/delete/',
+                function () { }
+            );
+        })
+        .on('ctx:edit', function (event) {
+            window.open('/character_sheet?sheet=' + $(event.delegateTarget).attr('data-cid'), '_blank');
+        });
+}
+
+function draw_objects() {
+    var objects = [];
+    var object_keys = Object.keys(current_map_data.objects);
+    for (var o = 0; o < object_keys.length; o++) {
+        var obj = current_map_data.objects[object_keys[o]];
+        switch (obj.type) {
+            case 'obscure':
+                objects.push(draw_obscure(obj));
+                break;
+            case 'note':
+                objects.push(draw_note(obj));
+                break;
+            case 'shape':
+                objects.push(draw_shape(obj));
+                break;
+            case 'character':
+                objects.push(draw_character(obj));
+                break;
+        }
+    }
+    return objects;
+}
---
+++ client/scripts/player_utils.js
@@ -0,0 +1,500 @@
+// Globals
+var params = {
+    campaign: null,
+    map: null
+};
+var uid = null;
+var current_cmp_data = {
+    owner: '',
+    id: '',
+    settings: {
+        rules: {
+            variant_encumbrance: false
+        }
+    },
+    name: '',
+    maps: {},
+    homebrew_creatures: [],
+    characters: [],
+    dms: [],
+    players: [],
+    _update: {
+        self: false,
+        maps: false
+    },
+    icon: null
+};
+var current_map_data = {
+    campaign: '',
+    id: '',
+    objects: {},
+    name: '',
+    map_img: '',
+    dimensions: {
+        scale: 0,
+        columns: 0,
+        rows: 0
+    },
+    initiative: {
+        combatants: {},
+        current: null,
+        active: false
+    },
+    _update: {
+        self: false
+    }
+};
+var current_tool = 'move';
+var cursor_tool_map = {
+    '#map-container img, .obscure': {
+        move: 'move',
+        obscure: 'crosshair',
+        draw: 'crosshair',
+        shape: 'crosshair',
+        measure: 'crosshair',
+        note: 'text'
+    }
+};
+var note_colors = [
+    'white',
+    'black',
+    'red',
+    'blue',
+    'green',
+    'pink',
+    'lightBlue',
+    'limeGreen',
+    'gray',
+    'lightGray'
+];
+
+// Panning/Zooming constants
+var scale = 1,
+    panning = false,
+    xoff = 0,
+    yoff = 0,
+    start = { x: 0, y: 0 },
+    scale_max = 20,
+    scale_min = 0.05
+function setTransform(element) {
+    $(element).css('transform', "translate(" + xoff + "px, " + yoff + "px) scale(" + scale + ")");
+}
+function getPercentPosition(x, y) {
+    return {
+        x: (x - $('#map-container').offset().left) / $('#map-container').width() * 100 / scale,
+        y: (y - $('#map-container').offset().top) / $('#map-container').height() * 100 / scale
+    }
+}
+function evGetPercentPosition(e) {
+    return getPercentPosition(e.clientX, e.clientY);
+}
+
+// Measuring vars
+var measure_start = { x: 0, y: 0 };
+var measuring = false;
+var measure_timeout = null;
+
+// Selection box
+var selector = {
+    start: {
+        x: 0,
+        y: 0
+    },
+    end: {
+        x: 0,
+        y: 0
+    },
+    type: null
+};
+
+function isOverlap(div1, div2) {
+    // Div 1 data
+    var d1_offset = $(div1).position();
+    var d1_height = $(div1).outerHeight(true);
+    var d1_width = $(div1).outerWidth(true);
+    var d1_distance_from_top = d1_offset.top + d1_height;
+    var d1_distance_from_left = d1_offset.left + d1_width;
+
+    // Div 2 data
+    var d2_offset = $(div2).position();
+    var d2_height = $(div2).outerHeight(true);
+    var d2_width = $(div2).outerWidth(true);
+    var d2_distance_from_top = d2_offset.top + d2_height;
+    var d2_distance_from_left = d2_offset.left + d2_width;
+
+    var not_colliding = (
+        d1_distance_from_top < d2_offset.top ||
+        d1_offset.top > d2_distance_from_top ||
+        d1_distance_from_left < d2_offset.left ||
+        d1_offset.left > d2_distance_from_left
+    );
+
+    // Return whether it IS colliding
+    return !not_colliding;
+};
+
+function getRect(x1, y1, x2, y2) {
+    if (x2 >= x1 && y2 >= y1) {
+        var top = y1;
+        var left = x1;
+        var width = x2 - x1;
+        var height = y2 - y1;
+    } else if (x2 < x1 && y2 >= y1) {
+        var top = y1;
+        var left = x2;
+        var width = x1 - x2;
+        var height = y2 - y1;
+    } else if (x2 >= x1 && y2 < y1) {
+        var top = y2;
+        var left = x1;
+        var width = x2 - x1;
+        var height = y1 - y2;
+    } else {
+        var top = y2;
+        var left = x2;
+        var width = x1 - x2;
+        var height = y1 - y2;
+    }
+    return {
+        top: top,
+        left: left,
+        width: width,
+        height: height
+    };
+}
+
+function getAngle(start, end) {
+    var angle = Math.atan((end.y - start.y) / (end.x - start.x));
+    if (end.y <= start.y && end.x >= start.x) { // Quadrant II
+        angle += Math.PI;
+    } else if (end.y >= start.y && end.x >= start.x) { // Quadrant III
+        angle += Math.PI;
+    } else if (end.y >= start.y && end.x <= start.x) { // Quadrant IV
+        angle += Math.PI * 2;
+    }
+    if (angle == 2 * Math.PI) {
+        angle = 0;
+    }
+
+    return angle;
+}
+
+function getCollisions(main, selector) {
+    var cols = $(selector).toArray().map(function (v, i, a) {
+        return [v, isOverlap(main, v)];
+    });
+    var rcols = [];
+    for (var i = 0; i < cols.length; i++) {
+        if (cols[i][1]) {
+            rcols.push(cols[i][0]);
+        }
+    }
+    return rcols;
+}
+function startSelector(e, type) {
+    var pos = evGetPercentPosition(e);
+    selector = {
+        start: {
+            x: pos.x,
+            y: pos.y
+        },
+        end: {
+            x: 0,
+            y: 0
+        },
+        type: cond(type == undefined, 'rectangle', type)
+    };
+    $('#selection-box')
+        .addClass('selecting')
+        .addClass(selector.type)
+        .css({
+            top: selector.start.y + '%',
+            left: selector.start.x + '%',
+            width: '0%',
+            height: '0%'
+        });
+}
+function updateSelector(e) {
+    if ($('#selection-box').hasClass('selecting')) {
+        selector.end = evGetPercentPosition(e);
+        if (selector.end.x >= selector.start.x && selector.end.y >= selector.start.y) {
+            var top = selector.start.y;
+            var left = selector.start.x;
+            var width = selector.end.x - selector.start.x;
+            var height = selector.end.y - selector.start.y;
+        } else if (selector.end.x < selector.start.x && selector.end.y >= selector.start.y) {
+            var top = selector.start.y;
+            var left = selector.end.x;
+            var width = selector.start.x - selector.end.x;
+            var height = selector.end.y - selector.start.y;
+        } else if (selector.end.x >= selector.start.x && selector.end.y < selector.start.y) {
+            var top = selector.end.y;
+            var left = selector.start.x;
+            var width = selector.end.x - selector.start.x;
+            var height = selector.start.y - selector.end.y;
+        } else {
+            var top = selector.end.y;
+            var left = selector.end.x;
+            var width = selector.start.x - selector.end.x;
+            var height = selector.start.y - selector.end.y;
+        }
+
+        $('#selection-box')
+            .css({
+                top: top + '%',
+                left: left + '%',
+                width: width + '%',
+                height: height + '%'
+            });
+        return {
+            top: top,
+            left: left,
+            width: width,
+            height: height
+        };
+    }
+    return {
+        top: 0,
+        left: 0,
+        width: 0,
+        height: 0
+    };
+}
+function finishSelector() {
+    var collisions = getCollisions('#selection-box', '.object');
+    $('#selection-box').removeClass('selecting');
+    var s_copy = JSON.parse(JSON.stringify(selector));
+    if (selector.end.x >= selector.start.x && selector.end.y >= selector.start.y) {
+        var top = selector.start.y;
+        var left = selector.start.x;
+        var width = selector.end.x - selector.start.x;
+        var height = selector.end.y - selector.start.y;
+    } else if (selector.end.x < selector.start.x && selector.end.y >= selector.start.y) {
+        var top = selector.start.y;
+        var left = selector.end.x;
+        var width = selector.start.x - selector.end.x;
+        var height = selector.end.y - selector.start.y;
+    } else if (selector.end.x >= selector.start.x && selector.end.y < selector.start.y) {
+        var top = selector.end.y;
+        var left = selector.start.x;
+        var width = selector.end.x - selector.start.x;
+        var height = selector.start.y - selector.end.y;
+    } else {
+        var top = selector.end.y;
+        var left = selector.end.x;
+        var width = selector.start.x - selector.end.x;
+        var height = selector.start.y - selector.end.y;
+    }
+    selector = {
+        start: {
+            x: 0,
+            y: 0
+        },
+        end: {
+            x: 0,
+            y: 0
+        },
+        type: null
+    };
+    return {
+        selection: s_copy,
+        dimensions: {
+            top: top,
+            left: left,
+            width: width,
+            height: height
+        },
+        collisions: collisions,
+        type: s_copy.type
+    };
+}
+
+function ftToPercentDimensions(w,h) {
+    return {
+        w: w / (current_map_data.dimensions.columns * current_map_data.dimensions.scale)*100,
+        h: h / (current_map_data.dimensions.rows * current_map_data.dimensions.scale)*100
+    };
+}
+
+var size_map = {
+    tiny: 2.5,
+    small: 5,
+    medium: 5,
+    large: 10,
+    huge: 15,
+    gargantuan: 20
+};
+
+// Context Menus
+var custom_ctx = {
+    delete: {
+        dms: {
+            classes: [
+                {
+                    match_type: 'any',
+                    match: [
+                        'object'
+                    ]
+                }
+            ]
+        },
+        players: {
+            classes: [
+                {
+                    match_type: 'all',
+                    match: [
+                        'character',
+                        'owned'
+                    ]
+                },
+                {
+                    match_type: 'any',
+                    match: [
+                        'shape'
+                    ]
+                }
+            ]
+        }
+    },
+    visibility_off: {
+        dms: {
+            classes: [
+                {
+                    match_type: 'all',
+                    match: [
+                        'note',
+                        'player-visible'
+                    ]
+                },
+                {
+                    match_type: 'all',
+                    match: [
+                        'shape',
+                        'player-visible'
+                    ]
+                }
+            ]
+        },
+        players: {}
+    },
+    visibility_on: {
+        dms: {
+            classes: [
+                {
+                    match_type: 'all',
+                    match: [
+                        'note',
+                        'player-invisible'
+                    ]
+                },
+                {
+                    match_type: 'all',
+                    match: [
+                        'shape',
+                        'player-invisible'
+                    ]
+                }
+            ]
+        },
+        players: {}
+    },
+    color_cycle: {
+        dms: {
+            classes: [
+                {
+                    match_type: 'any',
+                    match: [
+                        'note',
+                        'shape'
+                    ]
+                }
+            ]
+        },
+        players: {
+            classes: [
+                {
+                    match_type: 'any',
+                    match: [
+                        'note',
+                        'shape'
+                    ]
+                }
+            ]
+        }
+    },
+    add_character: {
+        dms: {
+            selector: [
+                {
+                    match_type: 'any',
+                    match: [
+                        '#map-container',
+                        '#map-container > img'
+                    ]
+                }
+            ],
+            predicate: [
+                {
+                    match_type: 'any',
+                    match: [
+                        'has_character_in_campaign'
+                    ]
+                },
+                {
+                    match_type: 'not any',
+                    match: [
+                        'all_characters_in_map'
+                    ]
+                }
+            ]
+        },
+        players: {
+            selector: [
+                {
+                    match_type: 'any',
+                    match: [
+                        '#map-container',
+                        '#map-container > img'
+                    ]
+                }
+            ],
+            predicate: [
+                {
+                    match_type: 'any',
+                    match: [
+                        'has_character_in_campaign'
+                    ]
+                },
+                {
+                    match_type: 'not any',
+                    match: [
+                        'all_characters_in_map'
+                    ]
+                }
+            ]
+        }
+    },
+    edit: {
+        dms: {
+            classes: [
+                {
+                    match_type: 'any',
+                    match: [
+                        'character'
+                    ]
+                }
+            ]
+        },
+        players: {
+            classes: [
+                {
+                    match_type: 'all',
+                    match: [
+                        'character',
+                        'owned'
+                    ]
+                }
+            ]
+        }
+    },
+};
